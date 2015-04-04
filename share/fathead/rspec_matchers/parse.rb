37f358b6d97c79900d4a5f06178ffa35206b492b
Peter Ching <peter.ching@gmail.com>
2015-04-04 15:41:02 -0400

    Scrape matchers section from Rspec's Github readme.

--- share/fathead/rspec_matchers/parse.rb
+++ share/fathead/rspec_matchers/parse.rb
@@ -1,3 +1,29 @@
 require 'nokogiri'
 
 page = Nokogiri::HTML(open("download/rspec-expectations"))
+
+# =============
+## DRIVER CODE
+# =============
+
+matchers_section = page.css("#readme > article > h2:nth-child(16)")[0]
+
+# p page.css("#readme > article > h2:nth-child(16)")[0].next_element.text
+
+def scrape_rspec_matchers (matchers_section)
+	current_elem = matchers_section.next_element
+
+	headings = []
+	code_samples = []
+
+	until current_elem.name == "h4"
+		headings << current_elem.text.strip if current_elem.name == "h3"
+
+		code_samples << current_elem.child.text if current_elem.name == "div"
+
+		current_elem = current_elem.next_element
+	end 
+
+	p headings.zip(code_samples)
+
+end
